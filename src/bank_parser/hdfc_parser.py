6832c81f4e5ac34a514fb5997ae143b3527511c3
Abhishek Saxena <as7122000@gmail.com>
2024-04-07 23:33:35 +0530

    feat: gen IDs for blank refs, deal with multiple files

--- src/bank_parser/hdfc_parser.py
+++ src/bank_parser/hdfc_parser.py
@@ -1,16 +1,31 @@
 import pandas as pd
 from src.utils import get_logger
 import re
+import hashlib
 
 log = get_logger(__name__)
 
 
+# Function to generate unique identifier
+def generate_unique_id(row):
+    # Concatenate values from all columns into a single string
+    row_string = "".join(str(value) for value in row)
+    print(row_string)
+    log.debug(f"Genrating hash for: {row['Narration']}")
+    # Generate hash value using SHA-256 hash function
+    hash_value = hashlib.sha256(row_string.encode()).hexdigest()
+    return hash_value
+
+
 class HdfcExcelDataReader:
     def __init__(self, file_paths: list[str]):
-        self.file_paths = file_paths
-        self.file_path = self.file_paths[0]
+        if len(file_paths) == 0:
+            raise ValueError("No file paths provided.")
+        self.file_paths = [
+            file_path for file_path in file_paths if "old" not in file_path
+        ]
 
-    def read_data(self, sheet_name=0) -> pd.DataFrame:
+    def read_data(self) -> pd.DataFrame:
         """
         Read data from the specified sheet of the Excel file, starting from
         the row after the header row,
@@ -25,15 +40,27 @@ class HdfcExcelDataReader:
         rows containing stars.
         """
         try:
-            all_data = self._read_all_data(sheet_name)
-            start_row = self._find_start_row(all_data)
-            end_row = self._find_end_row(all_data, (start_row or 0) + 1)
+            transaction_tables = []
+            for file_path in self.file_paths:
+                all_data = self._read_all_data(file_path)
+                start_row = self._find_start_row(all_data)
+                end_row = self._find_end_row(all_data, (start_row or 0) + 1)
 
-            if start_row is not None and end_row is not None:
-                df = self._extract_table_data(sheet_name, start_row, end_row)
-                return df
-            else:
-                raise ValueError("Could not find start and/or end row of the table.")
+                if start_row is not None and end_row is not None:
+                    log.debug(f"Start row: {start_row}")
+                    log.debug(f"End row: {end_row}")
+                    transaction_tables.append(
+                        self._extract_table_data(file_path, start_row, end_row)
+                    )
+                    log.debug(
+                        f"{len(transaction_tables[-1])} Transactions read from file: {file_path}"
+                    )
+                else:
+                    raise ValueError(
+                        "Could not find start and/or end row of the table."
+                    )
+
+            return self._combine_dataframes(transaction_tables)
         except Exception as e:
             raise Exception(f"An error occurred while reading the Excel file: {e}")
 
@@ -47,7 +74,6 @@ class HdfcExcelDataReader:
         }
 
         extracted_info = {}
-
         for key, pattern in patterns.items():
             match = re.search(pattern, narration)
             if match:
@@ -65,8 +91,8 @@ class HdfcExcelDataReader:
 
         return extracted_info
 
-    def _read_all_data(self, sheet_name):
-        return pd.read_excel(self.file_path, sheet_name=sheet_name, header=None)
+    def _read_all_data(self, file_path, sheet_name=0):
+        return pd.read_excel(file_path, sheet_name=sheet_name, header=None)
 
     def _find_start_row(self, all_data):
         for index, row in all_data.iterrows():
@@ -78,7 +104,7 @@ class HdfcExcelDataReader:
         for index, row in all_data.iterrows():
             if index > start_row and row.str.startswith("*").any():
                 # End at the row before the stars row after the table
-                return index - 2
+                return index - 1
         return None
 
     def _convert_to_datetime(self, df, columns):
@@ -100,9 +126,9 @@ class HdfcExcelDataReader:
             df[col] = df[col].fillna(fill_value)
         return df
 
-    def _extract_table_data(self, sheet_name, start_row, end_row):
+    def _extract_table_data(self, file_path, start_row, end_row, sheet_name=0):
         df = pd.read_excel(
-            self.file_path,
+            file_path,
             sheet_name=sheet_name,
             skiprows=start_row,
             nrows=end_row - start_row,
@@ -110,25 +136,43 @@ class HdfcExcelDataReader:
 
         df = df.drop(df.index[0])
 
-        # Convert to relevant data types
+        # Remove rows with all null values or stars
+        df = df.dropna(how="all")
 
+        # Convert to relevant data types
         df = self._convert_to_datetime(df, ["Date", "Value Dt"])
         df = self._convert_to_numeric(
             df, ["Withdrawal Amt.", "Deposit Amt.", "Closing Balance"]
         )
+        return df
 
-        # Remove rows with all null values or stars
-        df = df.dropna(how="all")
+    def _combine_dataframes(self, dfs):
+        combined_df = pd.concat(dfs)
+        combined_df.reset_index(drop=True, inplace=True)
+        combined_df.drop_duplicates(inplace=True)
 
-        df["ExtractedInfo"] = df["Narration"].apply(self.extract_narration_info)
+        empty_id_rows = combined_df["Chq./Ref.No."] == "0" * 15
+        combined_df.loc[empty_id_rows, "Chq./Ref.No."] = combined_df[
+            empty_id_rows
+        ].apply(generate_unique_id, axis=1)
 
-        df["Bank"] = "HDFC"
+        combined_df["_id"] = (
+            combined_df["Chq./Ref.No."]
+            + combined_df["Withdrawal Amt."].astype(str)
+            + combined_df["Deposit Amt."].astype(str)
+        )
 
-        df.rename(
+        combined_df.drop("Chq./Ref.No.", axis=1, inplace=True)
+
+        combined_df["ExtractedInfo"] = combined_df["Narration"].apply(
+            self.extract_narration_info
+        )
+
+        combined_df["Bank"] = "HDFC"
+
+        combined_df.rename(
             columns={
                 "Date": "ValueDate",
-                "Narration": "Narration",
-                "Chq./Ref.No.": "RefNo",
                 "Value Dt": "TransactionDate",
                 "Withdrawal Amt.": "WithdrawalAmt",
                 "Deposit Amt.": "DepositAmt",
@@ -137,4 +181,4 @@ class HdfcExcelDataReader:
             inplace=True,
         )
 
-        return df
+        return combined_df

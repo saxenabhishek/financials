fca382ca8c18a47a341460e49e83f24ed996b84c
Abhishek Saxena <as7122000@gmail.com>
2024-04-30 02:22:55 +0530

    feat: check for param value in query

--- src/static/toggle.js
+++ src/static/toggle.js
@@ -3,14 +3,12 @@ function toggleQueryParam(paramName, value) {
   const url = new URL(window.location.href);
   const params = new URLSearchParams(url.search);
 
-  if (params.has(paramName)) {
-    // Param already exists, remove it
+  if (params.has(paramName) && params.get(paramName) == value) {
     params.delete(paramName);
   } else {
     // Param doesn't exist, add it
     params.set(paramName, value);
   }
-
   url.search = params.toString();
   window.location.href = url.toString();
 }
